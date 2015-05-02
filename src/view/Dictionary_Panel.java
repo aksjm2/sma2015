758162ba8b4135d5f7fec2e47d635d17c16911bc
aksjm2 <aksjm2@gmail.com>
2015-05-02 16:27:32 +0900

    view 추가

---
+++ src/view/Dictionary_Panel.java
@@ -0,0 +1,15 @@
+package View;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class Dictionary_Panel implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/view/Main_Frame.java
@@ -0,0 +1,15 @@
+package View;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class Main_Frame extends JFrame implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
